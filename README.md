6a5fa20ab0135068acee54f108657206cfe9aca1
kgryte <kgryte@gmail.com>
2014-09-03 15:12:06 -0700

    [INIT]

---
+++ README.md
@@ -0,0 +1,86 @@
+Element
+===============
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Coverage][coveralls-image]][coveralls-url] [![Dependencies][dependencies-image]][dependencies-url]
+
+> Base class for minimal elements.
+
+
+## Installation
+
+``` bash
+$ npm install minimal-element-class
+```
+
+
+## Usage
+
+
+
+## Examples
+
+
+
+
+## Notes
+
+Inheriters should set Element `name` and whether if `void`.
+
+
+
+## Tests
+
+### Unit
+
+Unit tests use the [Mocha](http://visionmedia.github.io/mocha) test framework with [Chai](http://chaijs.com) assertions. To run the tests, execute the following command in the top-level application directory:
+
+``` bash
+$ make test
+```
+
+All new feature development should have corresponding unit tests to validate correct functionality.
+
+
+### Test Coverage
+
+This repository uses [Istanbul](https://github.com/gotwarlost/istanbul) as its code coverage tool. To generate a test coverage report, execute the following command in the top-level application directory:
+
+``` bash
+$ make test-cov
+```
+
+Istanbul creates a `./reports/coverage` directory. To access an HTML version of the report,
+
+``` bash
+$ open reports/coverage/lcov-report/index.html
+```
+
+
+## License
+
+[MIT license](http://opensource.org/licenses/MIT). 
+
+
+---
+## Copyright
+
+Copyright &copy; 2014. Athan Reines.
+
+
+
+[npm-image]: http://img.shields.io/npm/v/minimal-element-class.svg
+[npm-url]: https://npmjs.org/package/minimal-element-class
+
+[travis-image]: http://img.shields.io/travis/element-io/minimal-element-class/master.svg
+[travis-url]: https://travis-ci.org/element-io/minimal-element-class
+
+[coveralls-image]: https://img.shields.io/coveralls/element-io/minimal-element-class/master.svg
+[coveralls-url]: https://coveralls.io/r/element-io/minimal-element-class?branch=master
+
+[dependencies-image]: http://img.shields.io/david/element-io/minimal-element-class.svg
+[dependencies-url]: https://david-dm.org/element-io/minimal-element-class
+
+[dev-dependencies-image]: http://img.shields.io/david/dev/element-io/minimal-element-class.svg
+[dev-dependencies-url]: https://david-dm.org/dev/element-io/minimal-element-class
+
+[github-issues-image]: http://img.shields.io/github/issues/element-io/minimal-element-class.svg
+[github-issues-url]: https://github.com/element-io/minimal-element-class/issues
---
+++ test/test.js
@@ -0,0 +1,217 @@
+// MODULES //
+
+var // Expectation library:
+	chai = require( 'chai' ),
+
+	// Node utilities:
+	util = require( 'util' ),
+
+	// Module to be tested:
+	Element = require( './../lib' );
+
+
+// VARIABLES //
+
+var expect = chai.expect,
+	assert = chai.assert;
+
+
+// TESTS //
+
+describe( 'Element', function tests() {
+	'use strict';
+
+	// SETUP //
+
+	var element;
+
+	beforeEach( function() {
+		element = new Element();
+	});
+
+	// Mock elements...
+	function VoidElement() {
+		Element.call( this );
+		this._void = true;
+		return this;
+	}
+	VoidElement.prototype = Object.create( Element.prototype );
+	VoidElement.prototype.constructor = VoidElement;
+
+	function Div() {
+		Element.call( this );
+		this._name = 'div';
+		this._void = false;
+		return this;
+	}
+	Div.prototype = Object.create( Element.prototype );
+	Div.prototype.constructor = Div;
+
+
+	// TESTS //
+
+	it( 'should do export a constructor', function test() {
+		expect( Element ).to.be.an( 'object' );
+	});
+
+	describe( 'attr', function tests() {
+
+		it( 'should provide a method to set/get element attributes', function test() {
+			expect( element.attr ).to.be.a( 'function' );
+		});
+
+		it( 'should throw an error if provided a non-string attribute name', function test() {
+			var values = [
+					5,
+					true,
+					undefined,
+					null,
+					function(){},
+					{},
+					[],
+					NaN
+				];
+
+			for ( var i = 0; i < values.length; i++ ) {
+				expect( badValue( values[ i ] ) ).to.throw( TypeError );
+			}
+
+			function badValue( value ) {
+				return function() {
+					element.attr( value );
+				};
+			}
+		});
+
+		it( 'should throw an error if provided a non-string attribute name', function test() {
+			var values = [
+					5,
+					true,
+					undefined,
+					null,
+					function(){},
+					{},
+					[],
+					NaN
+				];
+
+			for ( var i = 0; i < values.length; i++ ) {
+				expect( badValue( values[ i ] ) ).to.throw( TypeError );
+			}
+
+			function badValue( value ) {
+				return function() {
+					element.attr( value );
+				};
+			}
+		});
+
+		it( 'should throw an error if provided a non-string/non-boolean/non-numeric attribute value', function test() {
+			var values = [
+					undefined,
+					null,
+					function(){},
+					{},
+					[],
+					NaN
+				];
+
+			for ( var i = 0; i < values.length; i++ ) {
+				expect( badValue( values[ i ] ) ).to.throw( TypeError );
+			}
+
+			function badValue( value ) {
+				return function() {
+					element.attr( value );
+				};
+			}
+		});
+
+		it( 'should set an attribute value', function test() {
+			element.attr( 'class', 'beep' );
+			assert.strictEqual( element.attr( 'class' ), 'beep' );
+		});
+
+		it( 'should return all attribute value pairs', function test() {
+			element
+				.attr( 'class', 'beep' )
+				.attr( 'property', 'beep' );
+
+			assert.deepEqual( element.attr(), {'class':'beep','property':'beep'} );
+		});
+
+	});
+
+	describe( 'isVoid', function tests() {
+
+		it( 'should provide a method to determine whether an element is a void element', function test() {
+			expect( element.isVoid ).to.be.a( 'function' );
+		});
+
+		it( 'should return whether an element is void', function test() {
+			assert.isBoolean( element.isVoid() );
+		});
+
+	});
+
+	describe( 'append', function tests() {
+
+		it( 'should provide a method to append elements to an element', function test() {
+			expect( element.append ).to.be.a( 'function' );
+		});
+
+		it( 'should throw an error if one tries to append to a void element', function test() {
+			var el = new VoidElement();
+			expect( foo ).to.throw( Error );
+			function foo() {
+				el.append( new Element() );
+			}
+		});
+
+		it( 'should throw an error if one tries to append a non-Element or non-TextNode', function test() {
+			expect( foo ).to.throw( Error );
+			function foo() {
+				el.append( new Date() );
+			}
+		});
+
+		it( 'should append elements', function test() {
+			var elements = [ new Div(), new Div() ];
+
+			elements[ 0 ].append( elements[ 1 ] );
+
+			assert.strictEqual( elements[0].toString(), '<div><div></div></div>' );
+		});
+
+	});
+
+	describe( 'toString', function tests() {
+
+		it( 'should provide a serialization method', function test() {
+			expect( element.toString ).to.be.a( 'function' );
+		});
+
+		it( 'should serialize an element as a string', function test() {
+			assert.strictEqual( element.toString(), '<></>' );
+		});
+
+		it( 'should serialize nested elements', function test() {
+			var el1 = new Div(),
+				el2 = new Div();
+
+			el1.attr( 'class', 'beep' );
+			el2.attr( 'class', 'boop' );
+
+			el1.append( el2 );
+
+			assert.strictEqual( el1.toString(), '<div class="beep"><div class="boop"></div></div>' );
+		});
+
+		it( 'should serialize void elements', function test() {
+			var el = new VoidElement();
+			assert.strictEqual( el.toString(), '</>' );
+		});
+
+	});
+
+});
